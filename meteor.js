58dfa3227b7fa14f8217ba801999fd85b3586299
msanjeetaxioned <sanjeetm@axioned.com>
2022-08-17 12:31:14 +0530

    Use Locomotive library for Smooth scrolling

--- meteor.js
+++ meteor.js
@@ -8,6 +8,27 @@ let meteorDefaultRotationSpeed; // 'true' for default rotation speed of Meteor
 let meteorClockwiseRotation; // contains the current direction the Meteor is rotating
 const rotation = gsap.timeline(); // GSAP timeline for Meteor rotation
 
+const locoScroll = new LocomotiveScroll({
+  el: container,
+  smooth: true
+});
+
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy(".container", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, 
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  pinType: container.style.transform ? "transform" : "fixed"
+});
+
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+ScrollTrigger.refresh();
+
 const scene = new THREE.Scene();
 let model = null;
 // const gui = new dat.GUI();
